de140149a6028b54daf44d9128cd3399dce656f8
Vasiliy Movchan <movchan.vasia@gmail.com>
2022-06-02 19:23:02 +0300

    create refs and add eventListener for task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const refs = {
+  body: document.querySelector('body'),
+  startButton: document.querySelector('[data-start]'),
+  stopButton: document.querySelector('[data-stop]'),
+};
 
+refs.startButton.addEventListener('click', () => {
+  refs.startButton.setAttribute('disabled', true);
+});
+
+refs.body.style.backgroundColor = getRandomHexColor();
+
+refs.stopButton.addEventListener('click', () => {
+  refs.startButton.removeAttribute('disabled');
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
