a516865469d551c38baa4551fe26964f4a557145
Vasiliy Movchan <movchan.vasia@gmail.com>
2022-06-04 17:29:24 +0300

    task-01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,16 +4,25 @@ const refs = {
   stopButton: document.querySelector('[data-stop]'),
 };
 
+let timerId = null;
+refs.stopButton.setAttribute('disabled', true);
+
 refs.startButton.addEventListener('click', () => {
   refs.startButton.setAttribute('disabled', true);
+  refs.stopButton.removeAttribute('disabled');
+  timerId = setInterval(changeBgColor, 1000);
 });
-
-refs.body.style.backgroundColor = getRandomHexColor();
 
 refs.stopButton.addEventListener('click', () => {
   refs.startButton.removeAttribute('disabled');
+  refs.stopButton.setAttribute('disabled', true);
+  clearInterval(timerId);
 });
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBgColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
 }
